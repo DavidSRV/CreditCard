3e7497b76ddab61d0dd86cfd29ea4f30afe94e91
David Ruiz <david.ruizvargas@hotmail.com>
2022-08-14 14:51:41 -0500

    Se agregan conroladores

---
+++ src/controllers/controllers.js
@@ -0,0 +1,22 @@
+// Controladores de input
+
+const user = document.getElementById("user");
+const number = document.getElementById("number");
+const cardNum = document.getElementById("cardNum");        
+const cardUser = document.getElementById("cardUser");
+const submit = document.getElementById("submit");
+const cvc = document.getElementById("cvc");
+const cardCvc = document.getElementById("cardCvc")
+
+
+submit.addEventListener("click",()=>{
+
+    window.addEventListener("submit",()=>{
+        cardNum.value = number.value;
+        cardUser.value = user.value;
+        cardCvc.value = cvc.value;
+        
+    })
+})
+
+

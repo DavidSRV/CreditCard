c974f5890faa6302d93da71289e51b57b8f794d0
David Ruiz <david.ruizvargas@hotmail.com>
2022-08-17 02:51:35 -0500

    Se crean alertas Span y Expresiones Regex

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,13 +1,31 @@
-// Controladores de input
+// INPUTS
+let user = document.getElementById("user");
+let number = document.getElementById("number");
+let cardNum = document.getElementById("cardNum");
+let cardUser = document.getElementById("cardUser");
+let button = document.getElementById("button");
+let cvc = document.getElementById("cvc");
+let cardCvc = document.getElementById("cardCvc");
+let form = document.getElementById("form");
 
-const user = document.getElementById("user");
-const number = document.getElementById("number");
-const cardNum = document.getElementById("cardNum");
-const cardUser = document.getElementById("cardUser");
-const button = document.getElementById("button");
-const cvc = document.getElementById("cvc");
-const cardCvc = document.getElementById("cardCvc")
-const form = document.getElementById("form")
+//ALERTAS SPAN
+let alertuser = document.getElementById("alertuser")
+let alertnumber = document.getElementById("alertnumber");
+let alertdate = document.getElementById("alertdate");
+let alertcvc = document.getElementById("alertcvc");
+
+
+//REGEX
+let validuser = /^(?!.* $)[A-Z][a-z ]+$||^(?!.* $)[A-Z][a-z ]+$/;
+// let validnumber = ;
+// let validdateday = ;
+// let validdatemonth = ;
+// let validcvc = ;
+
+
+
+
+
 
 
 function cancelar() {
@@ -15,16 +33,15 @@ function cancelar() {
 
     if (dato.value === "cancelar") {
         return true;
+
     } else {
-        
-            
 
         button.addEventListener("click", () => {
-        
+
             cardNum.value = number.value;
             cardUser.value = user.value;
             cardCvc.value = cvc.value;
-            
+
         })
         return false;
     }
